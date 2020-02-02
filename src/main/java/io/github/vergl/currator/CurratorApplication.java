f8869bf55362e95e7df51227e0a419d402b1a306
Stanislav Giley <vergl@MacBook-Pro-Stanislav.local>
2020-02-02 13:57:06 +0300

    initial commit

---
+++ src/main/java/io/github/vergl/currator/CurratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.vergl.currator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vergl/currator/CurratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vergl.currator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
