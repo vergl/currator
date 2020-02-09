faf63228cc239e16564fb0f9329666cea905b337
Stanislav Giley <stanislav.giley@gmail.com>
2020-02-09 22:56:10 +0300

    Add new Currency enumeration

---
+++ src/main/java/io/github/vergl/currator/domain/enumeration/Currency.java
@@ -0,0 +1,56 @@
+package io.github.vergl.currator.domain.enumeration;
+
+public enum Currency {
+    EUR("EU euro"),
+    USD("US dollar"),
+    JPY("Japanese yen"),
+    BGN("Bulgarian lev"),
+    CZK("Czech koruna"),
+    DKK("Danish krone"),
+    GBP("Pound sterling"),
+    HUF("Hungarian forint"),
+    PLN("Polish zloty"),
+    RON("Romanian leu"),
+    SEK("Swedish krona"),
+    CHF("Swiss franc"),
+    ISK("Icelandic krona"),
+    NOK("Norwegian krone"),
+    HRK("Croatian kuna"),
+    RUB("Russian rouble"),
+    TRY("Turkish lira"),
+    AUD("Australian dollar"),
+    BRL("Brazilian real"),
+    CAD("Canadian dollar"),
+    CNY("Chinese yuan renminbi"),
+    HKD("Hong Kong dollar"),
+    IDR("Indonesian rupiah"),
+    ILS("Israeli shekel"),
+    INR("Indian rupee"),
+    KRW("South Korean won"),
+    MXN("Mexican peso"),
+    MYR("Malaysian ringgit"),
+    NZD("New Zealand dollar"),
+    PHP("Philippine peso"),
+    SGD("Singapore dollar"),
+    THB("Thai baht"),
+    LTL("Lithuanian litas"),
+    LVL("Latvian lat"),
+    EEK("Estonian kroon"),
+    SKK("Slovak koruna"),
+    CYP("Cypriot pound"),
+    MTL("Maltese lira"),
+    SIT("Slovenian tolar"),
+    ROL("Romanian leu"),
+    TRL("Turkish lira (first)"),
+    ZAR("South African rand");
+
+    private String label;
+
+    private Currency(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
