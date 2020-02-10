8da3b4787057bd9c3d408bc1d8f7893709987d4c
Stanislav Giley <stanislav.giley@gmail.com>
2020-02-10 22:52:48 +0300

    Add constructor annotations

--- src/main/java/io/github/vergl/currator/domain/ecb/EcbRate.java
+++ src/main/java/io/github/vergl/currator/domain/ecb/EcbRate.java
@@ -2,9 +2,13 @@ package io.github.vergl.currator.domain.ecb;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import io.github.vergl.currator.domain.enumeration.Currency;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EcbRate {
     @JacksonXmlProperty(isAttribute = true)
     private Currency currency;
