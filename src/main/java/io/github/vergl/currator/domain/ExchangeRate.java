5edb8101a540727c442f5520262e139c409f793c
s.giley <s.giley@bi.zone>
2020-02-10 15:47:27 +0300

    Expand javax.persistence.*

--- src/main/java/io/github/vergl/currator/domain/ExchangeRate.java
+++ src/main/java/io/github/vergl/currator/domain/ExchangeRate.java
@@ -6,7 +6,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import java.util.Date;
 
 @Data
